713a486685ebc3b0fc75be29f2928f23e534c5b8
ashamahendiran <57298232+ashamahendiran@users.noreply.github.com>
2022-03-02 23:02:07 +0530

    Add files via upload

---
+++ src/Officepackages/go.mod
@@ -0,0 +1,3 @@
+module github.com/ashamahendiran/GoOfficedepo/src/Officepackages
+
+go 1.17
---
+++ src/Officepackages/office1.go
@@ -0,0 +1,11 @@
+package Officepackages
+
+func Subtraction(A int32, B int32) int32 {
+	X := B - A
+	return X
+}
+
+func Multiplication(A int32, B int32) int32 {
+	X := A * B
+	return X
+}
